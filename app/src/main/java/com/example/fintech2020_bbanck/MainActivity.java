779785702a653d63931553c81bad52ef907ccc66
dlthgml1997 <shfksthgml@naver.com>
2020-09-19 02:57:47 +0900

    feat: Add getBalance function

--- app/src/main/java/com/example/fintech2020_bbanck/MainActivity.java
+++ app/src/main/java/com/example/fintech2020_bbanck/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.fintech2020_bbanck.function.Alert;
 import com.example.fintech2020_bbanck.function.Register_fingerprint;
 import com.example.fintech2020_bbanck.function.Transfer;
 import com.example.fintech2020_bbanck.model.User;
@@ -21,24 +22,18 @@ public class MainActivity extends Activity {
 
     private Button btn_register;
     private Button btn_transfer;
+    private TextView tv_balance;
     private long backKeyPressedTime;
     private Toast toast;
 
-    @SuppressLint("ShowToast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        toast =  Toast.makeText(MainActivity.this, "\'뒤로\' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
+
         init();
         setOnBtnClickListener();
-        get_balance();
-    }
-
-    private void init() {
-        toast =  Toast.makeText(MainActivity.this, "'뒤로' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
-        btn_register = (Button)findViewById(R.id.btn_register);
-        btn_transfer= (Button)findViewById(R.id.btn_transfer);
     }
 
     private void setOnBtnClickListener() {
@@ -46,8 +41,12 @@ public class MainActivity extends Activity {
         btn_transfer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Transfer.class);
-                startActivity(intent);
+                if(tv_balance.getText().toString() == "0"){
+                    Alert.alert_function(MainActivity.this, "no_money");
+                }else{
+                    Intent intent = new Intent(getApplicationContext(), Transfer.class);
+                    startActivityForResult(intent,1004);
+                }
             }
         });
 
@@ -62,23 +61,26 @@ public class MainActivity extends Activity {
         });
     }
 
+    private void init() {
+        btn_register = (Button)findViewById(R.id.btn_register);
+        btn_transfer= (Button)findViewById(R.id.btn_transfer);
+        tv_balance =(TextView) findViewById(R.id.tv_balance);
+    }
+
     public void get_balance() {
-        SendRequest sendRequest = new SendRequest();
-        HashMap<String, String> hashMap = new HashMap<String, String>();
-        hashMap.put("session_key", User.getInstance().getSessionKey());
-        System.out.println("CHECK : "+hashMap);
-        sendRequest.send("https://"+ SSL_Connection.getSSLConnection().getUrl()+"/user/balance",
-                1, hashMap, MainActivity.this);
+        int balance = Integer.parseInt(tv_balance.getText().toString());
+        tv_balance.setText(String.valueOf(balance-1000));
     }
 
-    public void update_balance(String string) {
-//        TextView textView = (TextView)findViewById(R.id.tv);
-//        textView.setText(string+" 원");
-    }
-
-    public void onFinish() {
-        finish();
-        toast.cancel();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        {
+            if(resultCode == 1004) {
+                Alert.alert_function(MainActivity.this, "transfer");
+                get_balance();
+            } else {}
+        }
     }
 
     @Override
@@ -96,5 +98,10 @@ public class MainActivity extends Activity {
             sendRequest.send("https://"+ SSL_Connection.getSSLConnection().getUrl()+"/logout",
                     1, hashMap, MainActivity.this);
         }
+    }
+
+    public void onFinish() {
+        finish();
+        toast.cancel();
     }
 }
